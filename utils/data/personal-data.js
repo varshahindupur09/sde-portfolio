fb0e1f3c2b036ba70113efd8910386829a06f17f
Varsha Hindupur <hindupur.v@northeastern.edu>
2024-10-29 13:58:24 -0400

    modifications 2

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "VARSHA SESHASHAYEE HINDUPUR",
   profile: '/profile.jpg',
-  designation: "Software Developer and a Software Testing",
+  designation: "Software Engineer",
   description: "My name is Varsha. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'hindupur.v@northeastern.edu',
   phone: '+1 (857) 693-9108',
