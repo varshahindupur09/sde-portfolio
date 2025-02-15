12706ad2e220134b46d9064ad040dc7c717f1ba2
Varsha Hindupur <hindupur.v@northeastern.edu>
2025-02-15 13:21:33 -0600

    updation

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,9 +3,28 @@ import SystemDesignInitialArchitectureServerless from '/public/image/sysdesignIn
 
 export const experiences = [
   {
-    id: 1,
+    "id": 1,
+    "title": "Full Stack Developer",
+    "company": "Capital One",
+    "location": "Dallas, TX",
+    "duration": "(May 2023 – Present)",
+    "techstack": "React.js, Redux, Node.js, AWS (API Gateway, DynamoDB, EKS, ELK Stack, Terraform), OAuth 2.0, JWT, Redis, Jenkins, GitHub Actions, Docker",
+    "responsibilities": [
+        "Enhanced Capital One Rewards Redemption system using React/Redux, driving a 20% boost in user engagement and revenue growth.",
+        "Designed and implemented OAuth 2.0 and JWT-based authentication for APIs, minimizing unauthorized access incidents by 20% and ensuring secure user data handling.",
+        "Utilized AWS API Gateway to manage up to 500K API requests per day, maintaining high availability and performance for Credit Score Analysis and Rewards Redemption systems.",
+        "Optimized DynamoDB performance with indexing and Redis caching, enabling sub-100ms query responses and enhancing real-time analytics.",
+        "Automated CI/CD pipelines with Jenkins, GitHub Actions, and Docker, eliminating manual deployment errors and increasing release frequency.",
+        "Reduced infrastructure provisioning time by 70% via Terraform automation, standardizing cloud environments.",
+        "Standardized Kubernetes (EKS) infrastructure with Helm, cutting manual operations by 60% and upgraded system observability with the ELK Stack."
+    ],
+    "image1": "",
+    "github1": "https://github.com/varshahindupur09"
+  },
+  {
+    id: 2,
     title: "Full Stack Engineer",
-    company: "[RESEARCH] Northeastern University",
+    company: "[RESEARCH under Prof. Kelly Wellman] Northeastern University",
     location: "Boston, MA",
     duration: "(Feb 2024 – December 2024)",
     techstack: "MongoDB, Express.js, React.js 17, Node.js, FastAPI, Redux, AWS (CloudFront, S3, Beanstalk, CloudWatch), LLM, RAG pipleine",
@@ -22,18 +41,26 @@ export const experiences = [
     github1: "https://github.com/varshahindupur09/RA_Online_Instrument_Architecture/blob/main/README.md"
   },
   {
-    id: 2,
-    title: "Full Stack Engineer",
-    company: "IBM Global Services",
-    location: "Dallas, TX",
-    duration: "(May 2023 – Present)",
-    techstack: "Tech Stack: Python, asyncronous APIs, AWS, LangChain, CI/CD, GitHub Actions, Docker, Kubernetes",
-    responsibilities: [
-      "Developed web platform for diagnosing Parkinson’s disease using React.js, improving accessibility for healthcare professionals and achieving $8,000 in cost savings. Built HIPAA-compliant authentication workflows using OAuth 2.0, ensuring secure handling of sensitive data. Migrated static assets to AWS S3 and served via CloudFront CDN, reducing EC2 instance load and lowering content delivery costs by 25%. Integrated LangChain document retrieval system, boosting efficiency by 15% and cutting research time by 20%. Developed Meeting Intelligence application using Whisper API, GPT-3.5, AWS (Lambda and SQS) enabling real-time video transcription, chat, and automated summaries, improving post-meeting insights by 30%. Enhanced deployment and monitoring using AWS services (EC2, S3, CloudWatch, CloudFront), ensuring 99.9% uptime and fault-tolerant production environments. Developed unit and integration tests with Pytest, ensuring code quality and functionality while maintaining high level of reliability throughout development process."
-    ]
+    "id": 3,
+    "title": "Senior Software Engineer",
+    "company": "Deloitte",
+    "location": "Pune, India",
+    "duration": "(Mar 2019 – Aug 2022)",
+    "techstack": "Python, XGBoost, TensorFlow, Apache Airflow, PySpark, WebSocket, AWS (Lambda, EventBridge), FastAPI, Django, SQLAlchemy, Pytest, Selenium Grid",
+    "responsibilities": [
+        "Developed a credit risk ML model using XGBoost and TensorFlow, achieving a 92% accuracy rate and reducing false positives.",
+        "Automated 250K+ daily data processing with Apache Airflow & PySpark, eliminating manual workload for an entire week.",
+        "Engineered a real-time ML prediction system with WebSocket, AWS Lambda, and EventBridge, delivering up to $8,000 in monthly infrastructure savings.",
+        "Led a monolith-to-microservices migration, reducing deployment time from 4 hours to 30 minutes and enabling scalable real-time processing.",
+        "Optimized API performance by designing asynchronous backend services with FastAPI, Django, and SQLAlchemy ORM.",
+        "Introduced a TDD-driven test automation framework using Pytest and Selenium Grid, reducing defects by 30%.",
+        "Authored technical documentation for codebases, APIs, and system design, accelerating new developer onboarding by 2 weeks."
+    ],
+    "image1": "",
+    "github1": "https://github.com/varshahindupur09"
   },
   {
-    id: 3,
+    id: 4,
     title: "Data Scientist",
     company: "[RESEARCH] Northeastern University",
     location: "Boston, MA",
@@ -48,48 +75,22 @@ export const experiences = [
     ]
   },
   {
-    id: 4,
-    title: "Senior Software Engineer",
-    company: "Accenture",
-    location: "Pune, MH, India",
-    duration: "(Mar 2019 – Aug 2022)",
-    techstack: "Client: E-Commerce",
-    responsibilities: [
-      "Optimized server-side performance using Node.js, enabling real-time data processing and handling 10,000+ concurrent user requests. Created reusable Terraform configurations for AWS, reducing deployment errors by 15% and improving infrastructure consistency. Developed and maintained product browsing and cart modules within Salesforce Commerce Cloud (SFCC) SaaS e-commerce platform, reducing average page load time by 15%. Developed APIs and integrated third-party services (Stripe, Salesforce Service Cloud, and Order Management System) to streamline checkout flow, resulting in 30% improvement in checkout completion rate. Customized search experience using AWS Elasticsearch, improving search accuracy and driving 8% increase in conversion rates. Led team of 10 members for migration from monolithic MVC to microservices-based architecture, improving code modularity and maintainability by 40%.",
-      "Tech Stack: Salesforce Commerce Cloud (SFCC), Redis (ElastiCache), Elasticsearch, Jenkins (CI/CD), Maven (Build automation), SpringBoot Framework (4.3.x), MongoDB (Database), Terraform (Infrastructure-as-Code)."
-    ]
+    "id": 5,
+    "title": "Python Developer",
+    "company": "Capgemini",
+    "location": "Mumbai, India",
+    "duration": "(Mar 2017 – Mar 2019)",
+    "techstack": "Python, Pandas, NumPy, Django, GraphQL, Azure (Functions, Blob Storage), JavaScript, Agile Scrum",
+    "responsibilities": [
+        "Built a healthcare data processing system using Python, Pandas, and NumPy, improving workflow efficiency by 40%.",
+        "Engineered GraphQL APIs and optimized the Django backend, reducing payload size by 20% and improving response times by 300ms.",
+        "Launched a Health Information Exchange on Azure Functions and Blob Storage, enhancing system scalability and reducing deployment complexity.",
+        "Optimized the frontend through lazy loading, asset caching, and dynamic imports, cutting initial load time by 3.2 seconds.",
+        "Drove Agile Scrum adoption, reducing project delivery time and ensuring seamless feature delivery."
+    ],
+    "image1": "",
+    "github1": "https://github.com/varshahindupur09"
   },
-  {
-    id: 5,
-    title: "Software Engineer",
-    company: "Accenture",
-    location: "Mumbai City, MH, India",
-    duration: "(Mar 2019 – Dec 2020)",
-    techstack: "Client: Healthcare",
-    responsibilities: [
-      "Built health insurance portal front-end using React.js, leveraging CSS Grid for flexible layouts, and component-based architecture to enhance accessibility and reusability for 100,000+ policyholders. Developed Python scripts for automated data processing, reducing manual effort by 30% and improving operational efficiency. Designed and optimized MySQL database schemas, improving data retrieval efficiency by 25% and ensuring seamless backend performance. Mentored team of 4 junior engineers, fostering culture of code quality and best practices, resulting in 20% reduction in bug reports.",
-      "Tech Stack: Python, React.js (Frontend), Express.js-Node.js (Integration Layer)."
-    ]
-  },
-  // {
-  //   id: 6,
-  //   title: "Software Engineer",
-  //   company: "Infosys",
-  //   location: "Pune, MH, India",
-  //   duration: "(Mar 2017 – Mar 2019)",
-  //   techstack: "Client: Healthcare | Tech Stack: HTML, Tailwind CSS, React 16, REST APIs, Test Automation, OAuth 2.0",
-  //   responsibilities: [
-  //     "Developed a responsive patient portal front-end with React.js, using CSS Grid, component-based architecture and state management",
-  //     "Reduced user login time from 8 seconds to 3 seconds by implementing OAuth 2.0, ensuring a faster and smoother experience.",
-  //     "Developed robust end-to-end automated testing framework via Selenium, Cucumber, TestNG to reducing 40% manual testing effort.",
-  //     "Collaborated in client meetings, resulting to 20% rise in appreciations on healthcare app functionalities.",
-  //     "Significantly contributed to the team effort in remediating the log4j vulnerability across over 100 servers, enhancing the security posture."
-  //     // "Product: Loan Origination Platform for Northern Trust Bank",
-  //     // "Improved UI/UX loan approval features delivery time by 20% using React, Java SpringBoot and Material-UI, saving $100K annually.",
-  //     // "Designed an OAuth 2.0-based secure authentication and authorization system, enhancing API security by 20% across 50+ platforms.",
-  //     // "Diminished production defects by 40% via collaboration with QA and Data Science teams, leveraging automated testing and data-driven insights."
-  //   ]
-  // },
   {
     id: 7,
     title: "Game Developer (Android)",
@@ -104,13 +105,3 @@ export const experiences = [
     ]
   }
 ];
-
-
-// "Developed and optimized a Java-based Maven-TestNG-BDD framework for a live chatting application, reducing testing time by 90%.",
-//       "Set up Jenkins CI/CD pipelines, accelerating deployment speed and providing rapid feedback on code changes.",
-//       "Mentored junior engineers, reviewed code, and introduced new workflows using Jenkins, Maven, and BitBucket."
-
-// "Integrated Selenium with DevTest 10.1 for ISO 20022 messages, reducing production time by 70%.",
-// "Automated 500 test cases within a month using POM architecture for IMActions and TCS Bancs projects.",
-// "Managed PL/SQL scripts for efficient data extraction in aircraft document management systems for Boeing."
-// "Designed AWS QuickSight dashboards to validate and analyze user engagement via demographics and feedback, boosting data visibility by 30%."
